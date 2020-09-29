ef0cefb4c0990393a336912a0c3e9bf7b0797f92
AnaMariaBiliciuc <70842937+AnaMariaBiliciuc@users.noreply.github.com>
2020-09-29 18:17:13 +0300

    Add files via upload

---
+++ problema_7.py
@@ -0,0 +1,5 @@
+v=int(input("Introduceti varsta copilului"))
+g=2*v+8
+h=5*v+80
+print(" Greutatea ideala la varsta aceasta este ",g,"kg")
+print("Inaltimea ideala pentru varsta aceasta este ",h,"cm")
